65aeb09c10c0cd1bd0cd22ab4255c58aa53ef4a7
Júlio de Oliviera <ramosju423@gmail.com>
2024-10-14 23:49:37 -0300

    feat: Form is writing in the database

--- dados-pessoais/processa_form.php
+++ dados-pessoais/processa_form.php
@@ -1,24 +1,14 @@
 <?php
-// Configurações de conexão com o banco de dados
-$servername = "localhost";
-$username = "root";
-$password = "";  // Insira sua senha do MySQL
-$dbname = "vida_saude";
 
-// Criar a conexão
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificar a conexão
-if ($conn->connect_error) {
-    die("Conexão falhou: " . $conn->connect_error);
-}
+//* o require vai trazer tudo o que precisar do arquivo connection.php
+require "../connection.php";
 
 // Receber dados do formulário
-$nome = $_POST['nome'];
+$nome = $_POST['nome']; 
 $responsavel_legal = $_POST['nome-responsavel'];
 $naturalidade = $_POST['naturalidade'];
-$sexo = $_POST['genero'];
-$estado_civil = $_POST['estado_civil'];
+$sexo = $_POST['sexo']; 
+$estado_civil = $_POST['estado-c']; 
 $cpf = $_POST['cpf'];
 $data_nascimento = $_POST['data'];
 $contato = $_POST['contato'];
@@ -29,15 +19,12 @@ $numero = $_POST['numero'];
 $observacoes = $_POST['observacoes'];
 
 // Inserir dados na tabela
-$sql = "INSERT INTO dados_pessoais (nome,nome-responsavel, naturalidade, genero, estado_civil, cpf, data, contato, contato_emergencia, cidade, bairro, numero, observacoes)
-        VALUES ('$nome', '$responsavel_legal', '$naturalidade', '$sexo', '$estado_civil', '$cpf', '$data_nascimento', '$contato', '$contato_emergencia', '$cidade', '$bairro', '$numero', '$observacoes')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Dados inseridos com sucesso!";
-} else {
-    echo "Erro ao inserir os dados: " . $conn->error;
-}
+//* Para executar a query é bem simples, $variavel_conexão->query($querySQL); sem segredo
+$sql = "INSERT INTO dados_pessoais (nome,nome_responsavel, naturalidade, genero, estado_civil, cpf, data, contato, contato_emergencia, cidade, bairro, numero, observacoes)
+        VALUES ('$nome', '$responsavel_legal', '$naturalidade', '$sexo', '$estado_civil', '$cpf', '$data_nascimento', '$contato', '$contato_emergencia', '$cidade', '$bairro', '$numero', '$observacoes)";
+$conn->query($sql);
 
 // Fechar a conexão
 $conn->close();
-?>
+
+header("Location: ./dados-pessoais.html");
